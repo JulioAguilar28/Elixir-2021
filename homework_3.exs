13d9dfdfc7eec95419dc353bd41e587c3a5f586e
Julio Aguilar <julio.aguilar9928@gmail.com>
2021-11-25 10:32:36 -0600

    add gustavo's solution

--- homework_3.exs
+++ homework_3.exs
@@ -20,6 +20,8 @@
 #   "y" => 2
 # }
 
+# Solución Julio
+
 defmodule Homework3 do
   def frequencies(string) do
     string
@@ -38,3 +40,34 @@ defmodule Homework3 do
 
   defp add_first(map, key), do: Map.put(map, key, 1)
 end
+
+# Solución Gustavo
+
+defmodule TareaTres do
+  def countElem(map, valor) do
+    Enum.count(map, fn x -> x == valor end)
+  end
+
+  def frecuenciasFuncion(cadena, fun) do
+    enumerable = String.split(cadena)
+
+    enumerable
+    |> Enum.reduce(%{}, fn x, acc -> Map.put(acc, x, fun.(enumerable, x)) end)
+  end
+
+  def frecuencias(cadena) do
+    enumerable = String.split(cadena)
+
+    enumerable
+    |> Enum.reduce(%{}, fn x, acc -> Map.put(acc, x, countElem(enumerable, x)) end)
+  end
+end
+
+cadena =
+  "a los bunsancitos les gusta programar con otros bunsancitos y jugar con mas bunsancitos y con sus mascotas"
+
+TareaTres.countElem(String.split(cadena), "y")
+
+TareaTres.frecuencias(cadena)
+
+TareaTres.frecuenciasFuncion(cadena, &TareaTres.countElem/2)
